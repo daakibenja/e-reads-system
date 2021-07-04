344e3f489bf7fe76c5c308d83cc938421237d6c7
jjunju herman <jjunjuherman83@gmail.com>
2021-07-04 18:19:55 +0300

    initial commit

---
+++ config/routes.rb
@@ -0,0 +1,7 @@
+Rails.application.routes.draw do
+  root "books#index"
+  get "/books", to:"books#index"
+  # get "/articles", to: "articles#index"
+  # get "/articles/:id", to: "articles#show"
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+end
